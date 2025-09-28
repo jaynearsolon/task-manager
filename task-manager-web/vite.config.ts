e2359b3c1d07070ede4f4ba56a7cbcd6072d4846
jaynearsolon <araarsolon5@gmail.com>
2025-09-28 21:27:34 +0800

    Prepare for GitHub Pages deployment

--- task-manager-web/vite.config.ts
+++ task-manager-web/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // root, since you want it at https://jaynearsolon.github.io/
+});
